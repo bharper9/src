93ccae989f7b277a5424ea84c7e63713416cb4e2
bharper9 <bharper9@toromail.csudh.edu>
2024-03-27 00:58:22 -0700

    added vectors

--- tuple/tuple.c
+++ tuple/tuple.c
@@ -11,6 +11,17 @@ typedef struct tuple {
     double w;
 } Tuple;
 
+Tuple vector(double, double, double);
+Tuple addTuples(Tuple, Tuple);
+Tuple subTuples(Tuple, Tuple);
+Tuple negateTuple(Tuple);
+Tuple scalarMultTuple(double, Tuple);
+Tuple scalarDivideTuple(Tuple, double);
+double magnitudeOfVector(Tuple);
+Tuple normalizeVector(Tuple);
+double dotProduct(Tuple, Tuple);
+
+
 // Function to create a tuple
 Tuple tuple(double x, double y, double z, double w) {
     Tuple t = {x, y, z, w};
@@ -34,4 +45,46 @@ int main() {
     printf("a is not a vector: %s\n", is_vector(a) ? "false" : "true");
 
     return 0;
+    #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+    double w;
+} Tuple;
+
+Tuple vector(double x, double y, double z) {
+    Tuple vec;
+    vec.x = x;
+    vec.y = y;
+    vec.z = z;
+    vec.w = 0; // Since it's a vector, set w to 0
+    return vec;
 }
+
+bool is_point(Tuple t) {
+    return t.w == 1;
+}
+
+bool is_vector(Tuple t) {
+    return t.w == 0;
+}
+
+int main() {
+    // Test case
+    Tuple a = vector(4, -4, 3);
+
+    printf("Testing vector create:\n");
+    printf("a.x = %f\n", a.x);
+    printf("a.y = %f\n", a.y);
+    printf("a.z = %f\n", a.z);
+    printf("a.w = %f\n", a.w);
+
+    printf("Is a point? %s\n", is_point(a) ? "true" : "false");
+    printf("Is a vector? %s\n", is_vector(a) ? "true" : "false");
+
+    return 0;
+}
+
